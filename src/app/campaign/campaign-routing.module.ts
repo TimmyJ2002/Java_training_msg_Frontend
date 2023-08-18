3df460deda8d749120584f03d4de7a8a70fd0883
Zoltan Tovisi <Zoltan.Tovisi@msg.group>
2023-08-18 11:35:54 +0300

    Fixed dev

--- src/app/campaign/campaign-routing.module.ts
+++ src/app/campaign/campaign-routing.module.ts
@@ -5,23 +5,23 @@ import {CampaignCreateComponent} from "./component/campaign-create/campaign-crea
 import {CommonModule} from "@angular/common";
 import {CampaignEditComponent} from "./component/campaign-edit/campaign-edit.component";
 import {CampaignDeleteComponent} from "./component/campaign-delete/campaign-delete.component";
+import {CampaignReportingComponent} from "./campaign-reporting/campaign-reporting.component";
 
-const routes: Routes = [
+export const routes: Routes = [
+  { path: 'campaign-reporting', component: CampaignReportingComponent},
   {path: 'create', component: CampaignCreateComponent},
   {path: 'update', component: CampaignEditComponent},
   {path: 'delete', component: CampaignDeleteComponent}
 ];
 
-@NgModule({
-import {RouterModule, Routes} from "@angular/router";
-import {CampaignReportingComponent} from "./campaign-reporting/campaign-reporting.component";
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
+// @NgModule({
+// import {RouterModule, Routes} from "@angular/router";
+// import {CampaignReportingComponent} from "./campaign-reporting/campaign-reporting.component";
+// import {NgModule} from "@angular/core";
+// import {CommonModule} from "@angular/common";
 
 
-const routes: Routes = [
-  { path: 'campaign-reporting', component: CampaignReportingComponent}
-];
+
 
 @NgModule({
   declarations: [],
