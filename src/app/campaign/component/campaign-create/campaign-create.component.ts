2bd0b411befd39d94e17c95b30ea29a5a4a7c80e
Alexia Fratila <alexia.fratila@msg.group>
2023-08-25 11:20:13 +0300

    solved the create of the campaign with empty String

--- src/app/campaign/component/campaign-create/campaign-create.component.ts
+++ src/app/campaign/component/campaign-create/campaign-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormGroupDirective, Validators} from "@angular/forms";
 import {CampaignService} from "../../services/campaign.service";
 import {LanguageService} from "../../../services/language.service";
 
@@ -10,30 +10,31 @@ import {LanguageService} from "../../../services/language.service";
 })
 export class CampaignCreateComponent implements OnInit {
 
+  campaignForm: FormGroup;
   isSuccess: boolean = false;
   isDuplicate: boolean = false;
-  campaignForm: FormGroup;
   translatedMessage: string = '';
+
 
   constructor(private formBuilder: FormBuilder,
               private campaignService: CampaignService,
               private languageService: LanguageService) {
-    this.campaignForm = this.formBuilder.group( {
-      name:['', Validators.required],
+    this.campaignForm = this.formBuilder.group({
+      name: ['', Validators.required],
       purpose: ['', Validators.required]
     });
   }
 
-  onSubmit() {
+  onSubmit(formData: any, formDirective: FormGroupDirective) {
+    this.campaignForm.markAllAsTouched();
     if (this.campaignForm.valid) {
       const campaignData = this.campaignForm.value;
         this.campaignService.createCampaign(campaignData).subscribe(
           (response) => {
             console.log('Campaign created:', response);
             this.isSuccess = true;
+            formDirective.resetForm();
             this.campaignForm.reset();
-            this.campaignForm.controls['name'].setErrors(null);
-            this.campaignForm.controls['purpose'].setErrors(null);
           },
           (error) => {
             console.error('Error creating campaign: ', error);
@@ -54,16 +55,8 @@ export class CampaignCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.languageService.selectedLanguage$.subscribe((language) => {
-      // Fetch and set translated content based on the selected language
       this.translatedMessage = this.getTranslatedMessage(language);
     });
-  }
-
-  getErrorMessage() {
-    if (this.campaignForm.hasError('required')) {
-      return 'You must enter a value';
-    }
-    return this.campaignForm.hasError('name') ? 'Not a valid name' : '';
   }
 
   getTranslatedMessage(key: string): string {
